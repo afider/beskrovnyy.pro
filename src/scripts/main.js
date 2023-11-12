26f9cc12a850ab997b7b3cbba3eb0f99ec4243dc
afider <e.beskrovnyy@gmail.com>
2023-11-12 16:56:27 +0300

    added contents anchor logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,12 @@
 import switchTheme from '@js/modules/theme';
 import copy from '@js/modules/copy';
+import setContentsActiveState from '@js/modules/contents';
+
+onload = () => {
+  // ставим   scroll-behavior: smooth; только, когда загрузилась страница, а не готов документ, чтобы пользователи, у которых загрузился только документ уже смогли перейти по якорю
+  document.documentElement.style.scrollBehavior = "smooth";
+};
 
 switchTheme();
 copy();
+setContentsActiveState();
