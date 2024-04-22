92ef9982ba10a475645e068baeec11e0663cbad7
afider <e.beskrovnyy@gmail.com>
2024-04-22 18:41:40 +0300

    add contents styles, header js scroll logic

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,6 +2,7 @@ import switchTheme from '@js/modules/theme';
 import copy from '@js/modules/copy';
 import setContentsActiveState from '@js/modules/contents';
 import switchNavbar from '@js/modules/navbar';
+import setScrolledState from '@js/modules/scrolled';
 
 onload = () => {
   // ставим   scroll-behavior: smooth; только, когда загрузилась страница, а не готов документ, чтобы пользователи, у которых загрузился только документ уже смогли перейти по якорю
@@ -12,3 +13,4 @@ switchTheme();
 copy();
 setContentsActiveState();
 switchNavbar();
+setScrolledState();
